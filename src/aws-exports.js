2d6ae474f297d48c4d7abcf5d42bae6722db36f4
Sijie-Yang <98970172+Sijie-Yang@users.noreply.github.com>
2024-03-22 09:06:27 +0800

    Update
    update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://vzvk6monwvdhrcibudzfvewhqq.appsync-api.ap-southeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-6ybjk6sd6fhkfn2t3niogvdlzm"
+    "aws_appsync_apiKey": "da2-64jridupfnayfcr2qudlud3lua"
 };
 
 
