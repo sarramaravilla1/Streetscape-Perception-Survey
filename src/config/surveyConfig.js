87ac3fce12a8044dc1e146b07dfcfe901142779d
sarramaravilla1 <sarramaravilla@gmail.com>
2025-07-25 14:11:19 +0800

    Initial setup

--- src/config/surveyConfig.js
+++ src/config/surveyConfig.js
@@ -1,8 +1,8 @@
 export const surveyConfig = {
   // Basic Information
   title: "Urban Streetscape Perception Survey",
-  description: "Hello! Here at the Urban Analytics Lab at the National University of Singapore, we are currently conducting an online perception survey that will take approximately 15 minutes to complete and consists of around 50 questions regarding photos taken at the street-level. Such a survey will help us understand what drives people's perception and how to design spaces that are visually appealing. Throughout the survey, participants' main task is to select the most suitable street view image among multiple images based on certain dimensions and personal impressions. As a token of appreciation, participants who successfully complete the full survey will receive a compensation fee of SGD 5.",
-  
+  description: "Hello! I'm Sarra Maravilla from the University of the Philippines Diliman. I am currently conducting an online perception survey that will take approximately 15 minutes to complete and consists of around 50 questions regarding photos taken at the street-level. Such a survey will help us understand what drives people's perception and how to design spaces that are visually appealing. Throughout the survey, participants' main task is to select the most suitable street view image among multiple images based on certain dimensions and personal impressions. As a token of appreciation, participants who successfully complete the full survey will receive a compensation fee of Php 50.",
+
   // Lab Information - Replace with your own
   logo: "https://api.surveyjs.io/private/Surveys/files?name=8fb3943d-2606-4486-88ad-a41ad27f4570",
   logoPosition: "right",
